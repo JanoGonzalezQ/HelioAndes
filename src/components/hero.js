48d3993a72750421f1f36eec1fb54b6701f423da
JanoGonzalezQ <aleja.gonzalezq@duocuc.cl>
2025-10-27 21:45:45 -0300

    Servicios y Hero de ALe

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
 import logo2 from './imagenes/logo2.png';
 
 function BannerPrincipal() {
   return (
-    
     <div
       className='row mt-3'
       style={{
@@ -20,15 +19,29 @@ function BannerPrincipal() {
       <div className='col-lg-12'>
         <h1>Bienvenido a HELIOANDES</h1>
         <p>Energía solar accesible y confiable para tu hogar o pyme</p>
-        <p> </p>
-        <p> </p>
-        <p> </p>
-        href="/Ver Catalogo_HelioAndes_Energia.pdf"
+
+        <p className="mt-4">Ver catálogo HELIOANDES</p>
+        <div className="d-flex justify-content-center gap-3 mt-3">
+          {/* Botón para descargar el catálogo */}
+          <Button
+            variant="success"
+            href="/catalogo_helioandes.pdf"
+            download="catalogo_helioandes.pdf"
+          >
+            Descargar Catálogo
+          </Button>
+
+          {/* Botón para ir a la demo calculadora */}
+          <Button
+            variant="warning"
+            href="#demo-calculadora"
+          >
+            Ir a la Demo Calculadora
+          </Button>
+        </div>
       </div>
     </div>
   );
 }
 
 export default BannerPrincipal;
-
-
